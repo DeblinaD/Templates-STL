b9f00f4da3284611f1d3c370f56b3d93ca0dc975
Deblina Das <77379919+DeblinaD@users.noreply.github.com>
2021-09-06 21:27:31 +0530

    Create ContainerClass1.cpp

---
+++ ContainerClass1.cpp
@@ -0,0 +1,15 @@
+/*container class array*/
+#include<iostream>
+#include<array> /*this header file must be included */
+using namespace std;
+
+
+int main()
+{
+   array<int,5>a={10,4,7,9,3}; /*syntax is this*/
+
+   /*now printing of array*/
+   for(int num:a)
+   cout<<num<<" ";
+   return 0;
+}
